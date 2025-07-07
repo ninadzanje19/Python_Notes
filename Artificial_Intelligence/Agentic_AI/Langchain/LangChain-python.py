ce120cdcc27c2e152b3266b6d5ea7af118b93dab
ninadzanje19 <73586946+ninadzanje19@users.noreply.github.com>
2025-07-07 18:30:52 +0530

    Update LangChain-python.py (LCEL and Runnables added)

--- Artificial_Intelligence/Agentic_AI/Langchain/LangChain-python.py
+++ Artificial_Intelligence/Agentic_AI/Langchain/LangChain-python.py
@@ -10,6 +10,9 @@
 7. Agents
 """
 
+########################################################################################################################
+#                               Langchain Overview
+########################################################################################################################
 #pip install -U langchain-google-genai
 #For Google Gemini only
 
@@ -42,7 +45,7 @@ response = llm_gemini.invoke(messages)
 answer = textwrap.fill(response.content, width=100)
 
 ########################################################################################################################
-########################################################################################################################
+#                               Chains, Prompts and Loaders
 ########################################################################################################################
 
 """
@@ -56,6 +59,8 @@ These are:
 5. General Function     (Additional Functions used)
 """
 
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+"""Prompts"""
 #Langchain provides a powerful class PromptTemplate which basically modifies our simple given prompt
 from langchain.prompts import PromptTemplate
 
@@ -70,13 +75,16 @@ prompt = PromptTemplate(
     template=prompt_template
 )
 
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+"""Chains"""
 #Assemble the chain (prompt | llm)
 #The function happening here is we are setting up a chain that gives our prompt to our llm
 chain = prompt | llm_gemini
 
 response = chain.invoke({"topic":"What is Langchain"}).content
 
-
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+"""Loaders"""
 #In LangChain we are given inbuilt loaders class that helps us to load data for our application
 from langchain_community.document_loaders import csv_loader
 
@@ -96,7 +104,7 @@ chain = prompt | llm_gemini
 response_of_loaders = chain.invoke({"csv_transcript": docs}).content
 
 ########################################################################################################################
-########################################################################################################################
+#                        Langchain Expression Language and Runnables
 ########################################################################################################################
 
 """
@@ -117,3 +125,94 @@ RunnablePassthrough: Keeps the input unchanged or modifies the input that goes i
 RunnableParallel: Executes the multiple Runnable components concurrently. Allows us to branch the flow allowing us to 
                   get multiple outputs using single input.
 """
+
+from langchain.prompts import PromptTemplate
+
+#Standard process for creating a prompt
+summarization_template = ("You are a helpful assistant that summarizes AI concepts."
+                          "{context}"
+                          "Summarize the context")
+
+summarize_prompt = PromptTemplate.from_template(summarization_template)
+
+"Runnable Sequence"
+from langchain_core.output_parsers import StrOutputParser
+output_parser = StrOutputParser()
+chain = summarize_prompt | llm_gemini | output_parser           #Basic Runnable sequence
+
+response_of_runnable_sequence = chain.invoke({"context": "What is Langchain?"})
+
+"""
+Here the prompt is passed to the llm_gemini and the output is further passed to output_parser
+Which properly parses the output.
+
+Prompt -> Gemini processes the prompt -> Parses the prompt properly
+"""
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+"Runnable Lambda"
+from langchain_core.runnables import RunnableLambda
+
+#Create a lambda function and create a RunnableLambda object
+length_lambda = RunnableLambda(lambda summary_length: f"Summary length: {len(summary_length)} characters")
+
+#Chain the lambda function to the existing chain
+lambda_chain = summarize_prompt | llm_gemini | output_parser | length_lambda
+
+response_lambda = lambda_chain.invoke({"context": "What is Langchain?"})
+
+"""
+Here the existing chain's output's length is taken.
+Prompt -> Gemini processes the prompt -> Parses the prompt properly -> length is calculated
+"""
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+"Runnable Passthrough"
+from langchain_core.runnables import RunnablePassthrough
+
+#Create a RunnablePassthrough object
+passthrough = RunnablePassthrough()
+
+#Chain the passthrough object to the existing chain
+passthrough_chain_unchanged = summarize_prompt | llm_gemini | output_parser | passthrough
+
+response_passthrough_unchanged = passthrough_chain_unchanged.invoke({"context": "What is Langchain?"})
+
+"""
+Here the passthrough object is chained to the existing chain as a placeholder
+Prompt -> Gemini processes the prompt -> Parses the prompt properly -> placeholder(does nothing)
+"""
+
+#Define  lambda function that maps {"key" to value}
+passthrough_lambda_function = RunnableLambda(lambda summary: {"summary": summary})
+
+assign_passthruugh = RunnablePassthrough.assign(length = lambda x: len(x["summary"]))
+
+passthrough_chain_changed = summarize_prompt | llm_gemini | output_parser | passthrough_lambda_function | assign_passthruugh
+
+response_passthrough_changed = passthrough_chain_changed.invoke({"context": "What is Langchain?"})
+
+"""
+Here the passthrough objet is chained to the existing chain as a function that maps the summary and its length as {summary: , length: }
+We are modifying the output.
+Instead of the default return from output parser we change the output to a custom dictionary.
+Prompt -> Gemini processes the prompt -> Parses the prompt properly -> placeholder(maps the summary and its length as {summary: , length: })
+"""
+
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+"Runnable Parallel"
+from langchain_core.runnables import RunnableParallel
+
+parallel_runnable = RunnableParallel(
+                    summary=lambda x: x,
+                    length=lambda x: len(x)
+                    )
+
+parallel_chain = summarize_prompt | llm_gemini | output_parser | parallel_runnable
+
+response_parallel = parallel_chain.invoke({"context": "What is Langchain?"})
+
+########################################################################################################################
+#                        Splitters and Retrievers
+########################################################################################################################
