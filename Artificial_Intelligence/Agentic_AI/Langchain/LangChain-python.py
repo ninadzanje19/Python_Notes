88ee191b108e526f4a9e6dbe4c34d30cd86281db
ninadzanje19 <73586946+ninadzanje19@users.noreply.github.com>
2025-07-17 22:25:37 +0530

    Langchain rag added

--- Artificial_Intelligence/Agentic_AI/Langchain/LangChain-python.py
+++ Artificial_Intelligence/Agentic_AI/Langchain/LangChain-python.py
@@ -69,7 +69,7 @@ prompt_template = ("You are a helpful assistant that explains AI topics."
                    "Given the following input {topic}."
                    "Provide the explanation of the given topic")
 
-#Create prompt from prompt templates
+#Create prompt from prompt templates (This converts your simple text prompt to the format suitable for Langchain to process)
 prompt = PromptTemplate(
     input_variables=["topic"],                  #What is going to be your input
     template=prompt_template
@@ -244,15 +244,59 @@ vector_store = InMemoryVectorStore.from_documents(
     embeddings,
 )
 
-response_retriever = vector_store.similarity_search(query="What animals are found in the Savannah", k= 4)
+retrieved_results = vector_store.as_retriever(search_type="mmr", k= 4)
+response_retriever = retrieved_results.invoke("What is the flora of Savannah?")
+
 #Checkout various vector stores at https://python.langchain.com/docs/integrations/vectorstores/
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 ########################################################################################################################
 #                           RAG(Retrieval Argument Generation)
 ########################################################################################################################
+"""
+Process of RAG
+    1. Load the data using langchain loaders
+    2. Split the data into chunks using langchain splitters
+    3. Vectorize the chunks in the vector store
+    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+                Do this once in the first time only
+    4. Configure the langchain prompt runnable
+    5. Configure the llm runnable
+    6. Configure the output parser runnable
+    7. Configure the runnable lambda function if required for any additional functionality
+    8. Configure any passthroughs or parallels if required
+    9. Create the chain 
+        prompt -> retriever -> llm -> output_parser 
+"""
+
+#create a prompt runnable (component)
+from langchain.prompts import  ChatPromptTemplate
+rag_template = """
+                Answer the following question based upon the given context: {context}
+                Question: {question}
+               """
+
+rag_prompt = ChatPromptTemplate.from_template(rag_template)
+
+#Configure the llm runnable (component)
+rag_llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
 
 
+#create a output parser runnable (component)
+from langchain_core.output_parsers import StrOutputParser
+output_parser = StrOutputParser()
+
+rag_query = "What is the flora of Savannah"
+
+#Create the retriever runnable lambda (component)
+rag_retrieved_results = vector_store.as_retriever(search_type="mmr", k= 4)
+
+#Create the RAG chain
+rag_chain = ({"context": (lambda x: x["question"]) | rag_retrieved_results,
+              "question": (lambda x: x["context"])} | rag_prompt | rag_llm | output_parser
+
+)
+print(rag_chain.invoke(rag_query))
 ########################################################################################################################
 #                                               Tools
 ########################################################################################################################
