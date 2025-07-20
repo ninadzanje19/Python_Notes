04b35ba470f33395c81446d467833902d648ad19
ninadzanje19 <ninadzanje@rediffmail.com>
2025-07-21 03:09:44 +0530

    RAG complete

--- Artificial_Intelligence/Agentic_AI/Langchain/Langchain_RAG-python.py
+++ Artificial_Intelligence/Agentic_AI/Langchain/Langchain_RAG-python.py
@@ -11,11 +11,10 @@ Process of RAG
     4. Configure the langchain prompt runnable
     5. Configure the llm runnable
     6. Configure the output parser runnable
-    7. Configure the runnable lambda function if required for any additional functionality
-    8. Configure any passthroughs or parallels if required
-    9. Create the chain
+    7. Configure retriever and query as a runnable
+    8. Create the chain
         prompt -> retriever -> llm -> output_parser
-    10.Invoke the Chain
+    9.Invoke the Chain
 """
 
 #STEP 1 Load the data using langchain loaders
@@ -37,8 +36,8 @@ docs_split = text_splitter.split_documents(docs)
 
 
 #STEP 3 Create embeddings and Vectorize the chunks in the vector store
-from langchain_huggingface.embeddings import HuggingFaceEmbeddings
-embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+from langchain_huggingface import HuggingFaceEmbeddings
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
 from langchain_chroma import Chroma
 
@@ -63,8 +62,10 @@ vector_store.add_documents(
 from langchain.prompts import PromptTemplate
 
 #Create a prompt template
-prompt_template = ("You are a helpful assistant that gives answers to the questions asked"
-                   "{question}")
+prompt_template = """Answer the question based only on the following context:
+                     {context}
+                     Question: {question}
+                  """
 
 prompt = PromptTemplate(
     input_variables=["question"],
@@ -81,50 +82,13 @@ llm_gemini = ChatGoogleGenerativeAI(model="gemini-2.0-flash",
 from langchain_core.output_parsers import StrOutputParser
 output_parser = StrOutputParser()
 
-#STEP 7 Configure the runnable lambda function if required for any additional functionality
-from langchain_core.runnables import RunnableLambda
-retriever_lambda = RunnableLambda(lambda query: vector_store.as_retriever(search_type="mmr", search_kwargs={"k": 1}).invoke(query))
+#STEP 7 Configure retriever and query as a runnable
+rag_retriever = vector_store.as_retriever(search_type="mmr", search_kwargs={"k": 1})
+query_and_results = {"context": (lambda x: x["question"]) | rag_retriever, "question": (lambda x: x["question"])}
 
-#STEP 8 Configure any passthroughs or parallels if required
+#STEP 8 Create the chain (prompt -> retriever -> llm -> output_parser)
+chain = query_and_results | prompt | llm_gemini | output_parser
 
-#STEP 9 Create the chain (prompt -> retriever -> llm -> output_parser)
-chain = prompt | retriever_lambda
-"""| llm_gemini | output_parser"""
-
-#STEP 10 Invoke the Chain
-"""result = chain.invoke({"question": "What animals are found in Savanna?"})
-print(result)"""
-
-
-
-"""#create a prompt runnable (component)
-from langchain.prompts import  ChatPromptTemplate
-rag_template = 
-                Answer the following question based upon the given context: {context}
-                Question: {question}
-               
-
-rag_prompt = ChatPromptTemplate.from_template(rag_template)
-
-#Configure the llm runnable (component)
-from constants import gemini_api_key
-from langchain_google_genai import ChatGoogleGenerativeAI
-llm_gemini = ChatGoogleGenerativeAI(model="gemini-2.0-flash",
-                                    api_key=gemini_api_key)
-
-
-#create a output parser runnable (component)
-from langchain_core.output_parsers import StrOutputParser
-output_parser = StrOutputParser()
-
-rag_query = "What is the flora of Savannah"
-
-#Create the retriever runnable lambda (component)
-rag_retrieved_results = vector_store.as_retriever(search_type="mmr", k= 4)
-
-#Create the RAG chain
-rag_chain = ({"context": (lambda x: x["question"]) | rag_retrieved_results,
-              "question": (lambda x: x["context"])} | rag_prompt | rag_llm | output_parser
-
-)
-print(rag_chain.invoke(rag_query))"""
+#STEP 9 Invoke the Chain
+result = chain.invoke({"question": "What animals are found in Savanna?"})
+print(result)
